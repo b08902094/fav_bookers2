be4b5d9bce2c7225d3c60a4b469b15d5f833983d
b08902094 <72532740+b08902094@users.noreply.github.com>
2023-12-20 11:12:48 +0000

    [Finish] fav, comment

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,11 +1,16 @@
 class PostCommentsController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.post_comments.new(post_comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_to book_path(book)
+    @book = Book.find(params[:book_id])
+    @comment = current_user.post_comments.new(post_comment_params)
+    @comment.book_id = book.id
+    @comment.save
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    @book = Book.find(params[:id])
   end
 
   private
