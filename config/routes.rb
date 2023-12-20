7dfdd4e342337ea607e41df777ee6da664c20175
b08902094 <72532740+b08902094@users.noreply.github.com>
2023-12-20 16:20:51 +0000

    [Finish] comment

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,9 @@ Rails.application.routes.draw do
     resources :book_comments, only: [:create, :destroy]
   end
   get "home/about" => "homes#about", as: 'about'
-  resources :users, only: [:show, :edit, :update, :index]
+  resources :users, only: [:index, :show, :edit, :update] do
+    resource :relationships, only: [:create, :destroy]
+  	get "followings" => "relationships#followings", as: "followings"
+  	get "followers" => "relationships#followers", as: "followers"
+  end
 end
