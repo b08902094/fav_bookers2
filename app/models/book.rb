92bd73355ec3e7bcad2c3e7f2b53ac0d027a49d3
Marica Takahashi <b08902094@ntu.edu.tw>
2023-12-26 23:37:06 +0900

    [Finish] week overview

--- app/models/book.rb
+++ app/models/book.rb
@@ -7,6 +7,16 @@ class Book < ApplicationRecord
   validates :title, presence: true
   validates :body, length: { minimum: 1, maximum: 200 }
   has_many :view_counts, dependent: :destroy
+
+  scope :created_today, -> { where(created_at: Time.current.all_day) } 
+  scope :created_yesterday, -> { where(created_at: Time.current.yesterday.all_day) } 
+  scope :created_2day_ago, -> { where(created_at: 2.day.ago.all_day) }
+  scope :created_3day_ago, -> { where(created_at: 3.day.ago.all_day) }
+  scope :created_4day_ago, -> { where(created_at: 4.day.ago.all_day) }
+  scope :created_5day_ago, -> { where(created_at: 5.day.ago.all_day) }
+  scope :created_6day_ago, -> { where(created_at: 6.day.ago.all_day) }
+  scope :created_this_week, -> { where(created_at: Time.current.all_week) } 
+  scope :created_last_week, -> { where(created_at: Time.current.last_week.all_week) } 
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
   end
